408e3b5f43d2b8738a04b2fe1bb677f454d64a80
Peter Benavides <peter.r.benavides@gmail.com>
2016-01-25 04:21:18 -0800

    Fixed up styling on site

--- app/assets/javascripts/views/main/navbar.js
+++ app/assets/javascripts/views/main/navbar.js
@@ -3,6 +3,11 @@ FitsApp.Views.NavBar = Backbone.View.extend({
 
   className: "navigation-container",
 
+  events: {
+    "click #how-it-works-link":"scrollToSection",
+    "click #why-fits-link":"scrollToSection"
+  },
+
   initialize: function (options) {
     this.currentUser = options.currentUser;
   },
@@ -11,5 +16,13 @@ FitsApp.Views.NavBar = Backbone.View.extend({
     var content = this.template({currentUser: 'Peter'})
     this.$el.html(content);
     return this;
+  },
+
+  scrollToSection: function (event) {
+    event.preventDefault();
+    var targetId = event.currentTarget.id
+    $('html, body').animate({
+        scrollTop: $("." + targetId.slice(0, targetId.length - 5)).offset().top - 130
+    }, 500);
   }
 });
