8db08e5b26aae79d6a2c60d3d48d54ec5ca44b8d
Peter Benavides <peter.r.benavides@gmail.com>
2016-01-24 22:52:09 -0800

    Added backbone file structure

---
+++ app/assets/javascripts/fits_app.js
@@ -0,0 +1,13 @@
+window.FitsApp = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  FitsApp.initialize();
+});
