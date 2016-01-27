a17d652d2b091656db95af6ac6ed8885f8174cfa
Peter Benavides <peter.r.benavides@gmail.com>
2016-01-27 02:18:29 -0800

    Changed production email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => 'peter.r.benavides@gmail.com',
+   :password             => '3Point14',
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
